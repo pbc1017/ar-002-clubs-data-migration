7003e25f9c578a8de651e9d7d34621eccd17e8ff
perry <perry@channel.io>
2025-02-08 21:39:41 +0300

    chore: update main script to run funding migration instead of activity migration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 from app.activity.service.migration import migrate_activities
+from app.funding.service.migration import migrate_fundings
 
 if __name__ == "__main__":
-    asyncio.run(migrate_activities())
+    # asyncio.run(migrate_activities())
+    asyncio.run(migrate_fundings())
